73425c2a2015b6eaaa222bd4234771444bbce26e
Antonio Mallia <me@antoniomallia.it>
2018-04-13 18:59:43 -0400

    Read data

---
+++ FeatureBuilder.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import pandas as pd
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Feature Builder')
+    parser.add_argument('stances_dataset', metavar='stances_dataset',
+                        help='Stances dataset.')
+    parser.add_argument('bodies_dataset', metavar='bodies_dataset',
+                        help='Bodies dataset.')
+    parser.add_argument('output_file', metavar='output_file',
+                        help='Output file.')
+    args = parser.parse_args()
+
+    stances = pd.read_csv(args.stances_dataset, header=None)
+    bodies = pd.read_csv(args.bodies_dataset, header=None)
+
+
+
+if __name__ == '__main__':
+    main()
