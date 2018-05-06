838b54b144517672c378bf686b9975b9b7d4ba67
Melanie Tosik <melanie.tosik@gmail.com>
2018-05-05 21:07:55 -0400

    WIP

--- scripts/train_lda_model.py
+++ scripts/train_lda_model.py
@@ -23,7 +23,7 @@ for article in tqdm(random_articles, total=n):
     try:
         content = wikipedia.page(article).content
     except wikipedia.exceptions.DisambiguationError as e:
-        content = wikipedia.page(e.options[0]).content
+        continue
     prep = prep_text(content)
     data.append(prep)
 
