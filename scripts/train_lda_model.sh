8f2d890fdfe0a7d4c6a51ea3b00b3ebaabad596b
Melanie Tosik <tosik@nyu.edu>
2018-05-06 14:56:39 -0400

    add LDA script

---
+++ scripts/train_lda_model.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=4
+#SBATCH --time=48:00:00
+#SBATCH --mem=50GB
+#SBATCH --job-name=lda
+#SBATCH --mail-type=END
+#SBATCH --mail-user=tosik@nyu.edu
+#SBATCH --output=lda.log.txt
+
+source activate fnc
+
+python scripts/train_lda_model.py competition_test.yml
+
